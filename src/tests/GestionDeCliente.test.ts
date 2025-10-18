77599ff033e72235a66ae2cd66de469d870423a6
Nachit0C <nachociccone@gmail.com>
2025-10-18 20:28:10 -0300

    Sustituida clase Cliente por un objeto mock

--- src/tests/GestionDeCliente.test.ts
+++ src/tests/GestionDeCliente.test.ts
@@ -3,10 +3,20 @@ import ErrorClienteRepetido from "../AlquilerDeAutos/errors/excepcionClienteRepe
 import Cliente from "../AlquilerDeAutos/models/Cliente";
 import GestionDeClientes from "../AlquilerDeAutos/services/GestionDeCliente";
 
+const clienteMock = {
+    getNombre: jest.fn().mockReturnValue("nombre1"),
+    
+    getApellido: jest.fn().mockReturnValue("apellido1"),
+    
+    getEmail: jest.fn().mockReturnValue("email@email.com")
+} as unknown as Cliente;
+
 
 describe("Tests de la clase GestionDeClientes", ()=>{
-    const gestor = new GestionDeClientes();
-    const cliente1 = new Cliente("nombre1", "apellido1", "email1@email.com");
+    let gestor: GestionDeClientes;
+    beforeEach(()=>{
+        gestor = new GestionDeClientes();
+    });
     
     test("Verifica que se ejecuto correctamente el constructor de la clase, creando una instancia de GestionDeClientes: ", ()=>{
         expect(gestor).toBeInstanceOf(GestionDeClientes);
@@ -14,11 +24,11 @@ describe("Tests de la clase GestionDeClientes", ()=>{
     });
 
     test("Verifica el metodo agregarCliente: ", ()=>{
-        gestor.agregarCliente(cliente1);
-        expect(gestor["clientes"][0]).toEqual(cliente1);
+        gestor.agregarCliente(clienteMock);
+        expect(gestor["clientes"][0]).toEqual(clienteMock);
 
         try{
-            gestor.agregarCliente(cliente1);
+            gestor.agregarCliente(clienteMock);
         }catch(error){
             expect(error).toBeInstanceOf(ErrorClienteRepetido);
             expect(error.message).toEqual("El cliente ya esta en el sistema");
@@ -26,10 +36,11 @@ describe("Tests de la clase GestionDeClientes", ()=>{
     });
 
     test("Verifica el metodo eliminarCliente: ", ()=>{
-        gestor.eliminarCliente(cliente1);
+        gestor.agregarCliente(clienteMock);
+        gestor.eliminarCliente(clienteMock);
         expect(gestor["clientes"]).toEqual([]);
         try{
-            gestor.eliminarCliente(cliente1);
+            gestor.eliminarCliente(clienteMock);
         }catch(error){
             expect(error).toBeInstanceOf(ErrorClienteNoExiste);
             expect(error.message).toEqual("El cliente no esta en el sistema");
@@ -37,9 +48,9 @@ describe("Tests de la clase GestionDeClientes", ()=>{
     });
 
     test("Verifica el metodo clienteExiste: ", ()=>{
-        expect(gestor["clienteExiste"](cliente1)).toEqual(false);
-        gestor.agregarCliente(cliente1);
-        expect(gestor["clienteExiste"](cliente1)).toEqual(true);
+        expect(gestor["clienteExiste"](clienteMock)).toEqual(false);
+        gestor.agregarCliente(clienteMock);
+        expect(gestor["clienteExiste"](clienteMock)).toEqual(true);
     });
 
 });
